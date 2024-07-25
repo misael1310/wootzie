564347fe240eff47c55b04d2339e1c67fdac41f4
Misael <misaelperezoficial@gmail.com>
2024-07-25 14:55:10 -0400

    feat(routes): Bound Dashboard page component to Router component

--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { ProtectedRoute } from "./components/common/ProtectedRoute";
 import { routes } from "./utils/routes";
+import { Dashboard } from "./pages/Dashboard";
 const Router = () => {
   return (
     <BrowserRouter>
@@ -9,7 +10,7 @@ const Router = () => {
         <Route path={routes.homePage} element={<Login />} />
         <Route path={routes.loginPage} element={<Login />} />
         <Route element={<ProtectedRoute />}>
-          <Route path={routes.dashboardPage} element={<p>Dashboard...</p>} />
+          <Route path={routes.dashboardPage} element={<Dashboard />} />
         </Route>
         <Route path="*" element={<p>404 Not Found Page...</p>} />
       </Routes>
